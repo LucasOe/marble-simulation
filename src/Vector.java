370ceb13c075c7f7aa01c934a258db708c85a3cd
LucasOe <lucas.oelker@gmx.de>
2022-03-30 21:53:15 +0200

    Initial commit

---
+++ src/Vector.java
@@ -0,0 +1,29 @@
+public class Vector {
+    double x;
+    double y;
+
+    public Vector(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public Vector addVector(Vector vector) {
+        return new Vector(x + vector.getX(), y + vector.getY());
+    }
+
+    public Vector multiply(double factor) {
+        return new Vector(x * factor, y * factor);
+    }
+
+    public void print() {
+        System.out.println("X: " + x + ", Y: " + y);
+    }
+}
