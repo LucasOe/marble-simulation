062fc2bf1c38c9f9bc5b004ab4f6d483ba3c7000
LucasOe <lucas.oelker@gmx.de>
2022-03-31 11:15:21 +0200

    Add marble class

---
+++ src/Marble.java
@@ -0,0 +1,38 @@
+public class Marble {
+	private Vector position;
+	private Vector velocity;
+	private Vector influences;
+
+	public Marble(Vector startPosition, Vector startVelocity, Vector influences) {
+		this.position = startPosition;
+		this.velocity = startVelocity;
+		this.influences = influences;
+	}
+
+	public Vector getPosition() {
+		return position;
+	}
+
+	public Vector getVelocity() {
+		return velocity;
+	}
+
+	public void setPosition(Vector position) {
+		this.position = position;
+	}
+
+	public void setVelocity(Vector velocity) {
+		this.velocity = velocity;
+	}
+
+	public Vector calculateNewPos(double deltaTime) {
+		this.position = position.addVector(velocity.multiply(deltaTime)).addVector(influences.multiply(0.5)
+				.multiply(deltaTime * deltaTime));
+		return this.position;
+	}
+
+	public Vector calculateNewVel(double deltaTime) {
+		this.velocity = velocity.addVector(influences.multiply(deltaTime));
+		return this.velocity;
+	}
+}
