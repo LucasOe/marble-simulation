3c0e9972d44bd9dd718cbc8fa25471f0107ffd01
LucasOe <github@lucasoe.com>
2022-04-06 13:54:08 +0200

    Animate marble

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,14 +1,17 @@
 package app;
 
 import javafx.application.Application;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	private static double FPS = 60;
+	public static double FPS = 60;
 	public static int WIDTH = 1280;
 	public static int HEIGHT = 720;
 	public static int CANVAS_HEIGHT = 600;
+
+	private Gui gui;
 
 	public static void main(String[] args) throws Exception {
 		launch(args);
@@ -17,31 +20,30 @@ public class Main extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		// Start Values
-		Vector startPosition = new Vector(10, 50);
-		Vector startVelocity = new Vector(2, 1);
+		Vector startPosition = new Vector(10, 10);
+		Vector startVelocity = new Vector(50, 50);
 		Vector startInfluences = new Vector(0, -9.81);
 
 		Marble marble = new Marble(20, startPosition, startVelocity, startInfluences);
 
-		Gui gui = new Gui(stage);
-
-		gui.drawMarble(marble);
-
-		startLoop(marble);
+		gui = new Gui(stage);
+		Circle circle = gui.drawMarble(marble);
+		gui.startAnimationTimer(marble, circle);
 	}
 
-	private void startLoop(Marble marble) {
-		for (int i = 0; i < 5; i++) {
-			double deltaTime = 1 / FPS;
+	public static void updateMarble(Gui gui, Marble marble, Circle circle, int frame) {
+		double deltaTime = 1 / FPS;
 
-			// Calculates and return new position and velocity
-			Vector position = marble.calculateNewPos(deltaTime);
-			Vector velocity = marble.calculateNewVel(deltaTime);
+		// Calculates and return new position and velocity
+		Vector position = marble.calculateNewPos(deltaTime);
+		Vector velocity = marble.calculateNewVel(deltaTime);
 
-			// Print
-			System.out.println("Frame: " + i);
-			System.out.println("Pos: [x: " + position.getX() + ", y: " + position.getY() + "]");
-			System.out.println("Vel: [x: " + velocity.getX() + ", y: " + velocity.getY() + "]");
-		}
+		// Print
+		System.out.println("Frame: " + frame);
+		System.out.println("Pos: [x: " + position.getX() + ", y: " + position.getY() + "]");
+		System.out.println("Vel: [x: " + velocity.getX() + ", y: " + velocity.getY() + "]");
+
+		// Move position
+		gui.moveMarble(circle, marble);
 	}
 }
