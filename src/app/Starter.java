b658b13f7cd7411cda45d7c053720654d2681a2c
LucasOe <lucas.oelker@gmx.de>
2022-04-10 19:00:20 +0200

    Add Starter class with system properties

---
+++ src/app/Starter.java
@@ -0,0 +1,12 @@
+package app;
+
+public class Starter {
+	public static void main(String[] args) throws Exception {
+		System.setProperty("javafx.animation.pulse", "60");
+		System.setProperty("javafx.animation.fullspeed", "false");
+		System.setProperty("quantum.multithreaded", "false");
+		System.setProperty("prism.vsync", "false");
+
+		Main.main(args);
+	}
+}
